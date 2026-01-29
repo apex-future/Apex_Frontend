0ff9b395c7c3e1cb15dc22025c122c1315de0ff6
iom-future <isaacmoses11111@gmail.com>
2026-01-28 17:46:00 -1100

    Enhance HeroSection with updated text content and improved layout; adjust image styling for better presentation

--- src/components/landing-page/HeroSection.jsx
+++ src/components/landing-page/HeroSection.jsx
@@ -21,17 +21,20 @@ gsap.registerPlugin(TextPlugin);
             <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
 
         <div className='hero-section-content p-3 flex flex-col gap-5'>
-            <div className='text-conetnt'>
-                <h1 className='text-4xl sm:text-5xl font-bold leading-tight sm:leading-snug text-center font-display'>Study Smarter
+            <div className='text-content max-w-[800px] mx-auto'>
+                <h1 className='text-4xl sm:text-5xl md:text-6xl font-bold leading-tight sm:leading-snug text-center font-display'>Study Smarter
                     <br></br>
                     Reach Your <span className='inline-flex items-center'>
                                 <span className=' apex italic px-2 inline-block h-[1.3em] py-0 bg-accent-primary/30 '> </span>
                                 <span className='inline-block sm:w-1 w-[0.1em] h-[1em] sm:h-[1.2em] bg-accent-primary ml-1 animate-blink'></span>
                         </span> 
                 </h1>
+                <h3 className='text-center p-2 text-text-primary '>Stop juggling tabs to understand one chapter. Apex brings AI 
+explanations, definitions, and videos right where you're reading—
+so you can stay in your zone and learn deeply.</h3>
             </div>
-            <div className="img-content">
-                <img src={heroImg} alt="Apex dashboard" className="hero-img ml-auto" />
+            <div className="img-content  ">
+                <img src={heroImg} alt="Apex dashboard" className="hero-img mx-auto  shadow-lg" />
             </div>
         </div>
     </div>
