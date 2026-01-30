2ff6026d238a077a7f6a402ae7b831729e47235b
iom-future <isaacmoses11111@gmail.com>
2026-01-29 18:00:30 -1100

    add CTA buttons, design the hero section for better responsiveness

--- src/components/landing-page/HeroSection.jsx
+++ src/components/landing-page/HeroSection.jsx
@@ -15,12 +15,25 @@ gsap.registerPlugin(TextPlugin);
             duration:2
         })
         },[])
+        useGSAP(()=>{
+            gsap.fromTo(".hero-img",{
+                x: -200,
+         
+                duration:3,
+                ease: "circ.inOut"
+            },
+            {
+             
+                
+                x:0
+            })
+            },[])
          return (
     <div className='h-screen w-full relative pt-20'>
       
             <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
 
-        <div className='hero-section-content p-3 flex flex-col gap-5'>
+        <div className='hero-section-content p-3 flex flex-col gap-8'>
             <div className='text-content max-w-[800px] mx-auto'>
                 <h1 className='text-4xl sm:text-5xl md:text-6xl font-bold leading-tight sm:leading-snug text-center font-display'>Study Smarter
                     <br></br>
@@ -29,12 +42,20 @@ gsap.registerPlugin(TextPlugin);
                                 <span className='inline-block sm:w-1 w-[0.1em] h-[1em] sm:h-[1.2em] bg-accent-primary ml-1 animate-blink'></span>
                         </span> 
                 </h1>
-                <h3 className='text-center p-2 text-text-primary '>Stop juggling tabs to understand one chapter. Apex brings AI 
+                <h3 className='text-center p-2 text-text-primary sm:text-lg text-secondary md:text-xl'>Stop juggling tabs to understand one chapter. Apex brings AI 
 explanations, definitions, and videos right where you're reading—
-so you can stay in your zone and learn deeply.</h3>
+so you can stay focused</h3>
+
+                
+            </div>
+            <div className="CTA-buttons flex flex-col mx-auto w-[80%] sm:flex-row justify-center items-center gap-3">
+                <a href="" className='p-3 rounded-full w-full sm:w-1/2 max-w-[272px] border-2 px-5 text-center hover:bg-black hover:text-white text-white border-subtle bg-accent-primary font-medium md:text-lg '>Join the Waitlist</a>
+                <a className=" p-3 px-5  w-full text-center rounded-full max-w-[272px] shadow-md md:text-lg sm:w-1/2 font-medium border-2   border-subtle bg-bg-elevated  hover:bg-black ">Get a Demo</a>
+             
+
             </div>
             <div className="img-content  ">
-                <img src={heroImg} alt="Apex dashboard" className="hero-img mx-auto  shadow-lg" />
+                <img src={heroImg} alt="Apex dashboard" className="hero-img mx-auto hover:rotate-2 transition-all duration-1000  shadow-lg" />
             </div>
         </div>
     </div>
--- src/components/landing-page/NavBar.jsx
+++ src/components/landing-page/NavBar.jsx
@@ -13,8 +13,8 @@ function NavBar() {
         setAsideIsOpen(false)
     }
   return (
-    <div className='w-full nav-div p-3 fixed z-[100]'>
-        <nav className='w-[95%] justify-between p-2 mx-auto flex items-center backdrop-blur-md shadow-sm h-12 bg-bg-subtle rounded-full border-2 border-default'>
+    <div className='w-full nav-div p-3 py-4 fixed z-[100]'>
+        <nav className='w-[80%] justify-between p-2 mx-auto flex items-center backdrop-blur-md shadow-sm h-12 bg-bg-subtle rounded-full border-2 border-default'>
             <div>
                 <img  className="size-8 rounded-full" alt="apex-logo" src={logoLight} />
             </div>
