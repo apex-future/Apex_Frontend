a6b000ac523ac0584b0fce0f7bfb6bfb25951699
iom-future <isaacmoses11111@gmail.com>
2026-01-29 18:08:46 -1100

    animate hero section image

--- src/components/landing-page/HeroSection.jsx
+++ src/components/landing-page/HeroSection.jsx
@@ -17,25 +17,24 @@ gsap.registerPlugin(TextPlugin);
         },[])
         useGSAP(()=>{
             gsap.fromTo(".hero-img",{
-                x: -200,
-         
+                x: 200,
                 duration:3,
-                ease: "circ.inOut"
+            
             },
             {
-             
-                
+                ease: "bounce.out",
+                duration:3,
                 x:0
             })
             },[])
          return (
-    <div className='h-screen w-full relative pt-20'>
+    <div className='h-screen w-full relative pt-20 '>
       
             <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
 
         <div className='hero-section-content p-3 flex flex-col gap-8'>
             <div className='text-content max-w-[800px] mx-auto'>
-                <h1 className='text-4xl sm:text-5xl md:text-6xl font-bold leading-tight sm:leading-snug text-center font-display'>Study Smarter
+                <h1 className='text-4xl sm:text-5xl md:text-6xl font-bold leading-tight sm:leading-snug md:leading-normal text-center font-display'>Study Smarter
                     <br></br>
                     Reach Your <span className='inline-flex items-center'>
                                 <span className=' apex italic px-2 inline-block h-[1.3em] py-0 bg-accent-primary/30 '> </span>
@@ -54,8 +53,8 @@ so you can stay focused</h3>
              
 
             </div>
-            <div className="img-content  ">
-                <img src={heroImg} alt="Apex dashboard" className="hero-img mx-auto hover:rotate-2 transition-all duration-1000  shadow-lg" />
+            <div className="img-content overflow-hidden ">
+                <img src={heroImg} alt="Apex dashboard" className="hero-img rounded-xl mx-auto hover:rotate-3 transition-all duration-75  shadow-lg" />
             </div>
         </div>
     </div>
