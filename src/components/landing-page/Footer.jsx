b50d53015f3022f9d276cb72324a3015aeb84be1
iom-future <isaacmoses11111@gmail.com>
2026-02-08 04:11:55 -1100

    correct the date in the footer section; from 2025 to 2026

--- src/components/landing-page/Footer.jsx
+++ src/components/landing-page/Footer.jsx
@@ -99,7 +99,7 @@ function Footer() {
 
           {/* Bottom Bar */}
           <div className="border-t border-border-default/20 mt-12 pt-8 text-center text-sm text-text-dark-tertiary">
-            <p>&copy; 2025 Apex. All rights reserved.</p>
+            <p>&copy; 2026 Apex. All rights reserved.</p>
           </div>
         </div>
       </div>
