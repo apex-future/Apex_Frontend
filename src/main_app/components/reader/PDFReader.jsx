9f4e54ed1d5d193f51b42124178edd1173eb8fda
iom-future <isaacmoses11111@gmail.com>
2026-03-25 22:23:52 +0100

    fix the overflow-hidden effect on the left side of books when zoomed

--- src/main_app/components/reader/PDFReader.jsx
+++ src/main_app/components/reader/PDFReader.jsx
@@ -458,11 +458,13 @@ const PDFReader = ({
               return (
                 <div
                   key={virtualRow.index}
-                  className="pdf-page-wrapper absolute left-0 w-full flex flex-col items-center bg-bg-elevated"
+                  className="pdf-page-wrapper absolute left-0 flex flex-col items-center bg-bg-elevated"
                   data-page-index={pageIdx}
                   style={{
                     top: `${virtualRow.start}px`,
                     height: `${virtualRow.size}px`,
+                    width: 'max-content',
+                    minWidth: '100%',
                   }}
                 >
                   <VirtualPage
